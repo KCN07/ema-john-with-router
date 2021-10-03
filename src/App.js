f9d66b688298c6a46976ba450b60d13b8c671090
KCN007 <www.kaniz98@gmail.com>
2021-10-04 00:53:04 +0600

    finishing router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
+import NotFound from "./components/NotFound/NotFound";
+import OrderReview from "./components/OrderReview/OrderReview";
+import Inventory from "./components/Inventory/Inventory";
+import PlaceOrder from "./components/PlaceOrder/PlaceOrder";
 
 function App() {
   return (
     <div>
+      
+      <Router>
       <Header></Header>
-      <Shop></Shop>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="/placeorder">
+            <PlaceOrder></PlaceOrder>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
